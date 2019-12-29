f9b33a6bc495f710b7dc2568f84733ee8b07f6d8
shasssi <59342317+shasssi@users.noreply.github.com>
2019-12-30 00:13:22 +0530

    Create Binary-Serach.js
    binary search code

---
+++ Binary-Serach.js
@@ -0,0 +1,24 @@
+function binary_serach(arr, x, index){
+	let len = arr.length;
+	let mid = Math.floor(len/2)
+	if(x == arr[mid]){
+		return index + mid;
+	}
+	else if(x < arr[mid]){
+		let new_arr = arr.slice(0, mid);
+		return binary_serach(new_arr, x, index);
+	}
+	else if(x > arr[mid]){
+		let new_arr = arr.slice(mid+1, len);
+		index = index + mid + 1;
+		return binary_serach(new_arr, x, index);
+	}
+	else{
+		return -1
+	}
+	
+}
+
+let sorted_arr = [1,2,3,3,3,4,4,6,9,10]
+let res = binary_serach(sorted_arr, -1, 0)
+console.log(res)
