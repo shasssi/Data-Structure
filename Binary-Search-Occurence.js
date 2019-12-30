a3e148a8ee6a01b0c699b38c7c074cf4f13f475f
shasssi <59342317+shasssi@users.noreply.github.com>
2019-12-31 00:26:39 +0530

    Update Binary-Search-Occurence.js

--- Binary-Search-Occurence.js
+++ Binary-Search-Occurence.js
@@ -1,4 +1,6 @@
-let first_occurence = -1
+let first_occurence = -1;
+let last_occurence = -1;
+
 function binary_serach(arr, x, index){
 	let len = arr.length;
 	let mid = Math.floor(len/2)
@@ -7,7 +9,8 @@ function binary_serach(arr, x, index){
 	}
 	if(x == arr[mid]){
 		first_occurence = index + mid;
-		let new_arr = arr.slice(0, mid);
+        let new_arr = arr.slice(0, mid);
+        // last_occurence = index + mid;
 		// let new_arr = arr.slice(mid+1, len);
 		return binary_serach(new_arr, x, index);
 	}
@@ -23,6 +26,7 @@ function binary_serach(arr, x, index){
 	
 }
 
-let sorted_arr = [1,3,3,3,3,3,4,4,6,9,10]
-let res = binary_serach(sorted_arr, 3, 0)
-console.log(first)
+let sorted_arr = [1,3,3,3,3,3,4,4,6,9,10];
+let res = binary_serach(sorted_arr, 3, 0);
+console.log(first_occurence);
+// console.log(last_occurence);
