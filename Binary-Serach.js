3a4ee99c082f5e0d914d19da861d87d19b715e3d
shasssi <59342317+shasssi@users.noreply.github.com>
2019-12-30 23:55:56 +0530

    Update Binary-Serach.js

--- Binary-Serach.js
+++ Binary-Serach.js
@@ -1,6 +1,9 @@
 function binary_serach(arr, x, index){
 	let len = arr.length;
 	let mid = Math.floor(len/2)
+	if(arr.length === 0){
+		return -1
+	}
 	if(x == arr[mid]){
 		return index + mid;
 	}
@@ -13,10 +16,6 @@ function binary_serach(arr, x, index){
 		index = index + mid + 1;
 		return binary_serach(new_arr, x, index);
 	}
-	else{
-		return -1
-	}
-	
 }
 
 let sorted_arr = [1,2,3,3,3,4,4,6,9,10]
