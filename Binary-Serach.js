8beeca8dd26ee31b2ec63aa8883898d49d693444
shasssi <59342317+shasssi@users.noreply.github.com>
2024-03-03 00:22:02 +0530

    Update Binary-Serach.js

--- Binary-Serach.js
+++ Binary-Serach.js
@@ -1,23 +1,21 @@
-function binary_serach(arr, x, index){
-	let len = arr.length;
-	let mid = Math.floor(len/2)
-	if(arr.length === 0){
-		return -1
-	}
-	if(x == arr[mid]){
-		return index + mid;
-	}
-	else if(x < arr[mid]){
-		let new_arr = arr.slice(0, mid);
-		return binary_serach(new_arr, x, index);
-	}
-	else if(x > arr[mid]){
-		let new_arr = arr.slice(mid+1, len);
-		index = index + mid + 1;
-		return binary_serach(new_arr, x, index);
-	}
+function binary_serach(arr, x, index = 0) {
+  let len = arr.length;
+  let mid = Math.floor(len / 2);
+  if (arr.length === 0) {
+    return -1;
+  }
+  if (x == arr[mid]) {
+    return index + mid;
+  } else if (x < arr[mid]) {
+    let new_arr = arr.slice(0, mid);
+    return binary_serach(new_arr, x, index);
+  } else if (x > arr[mid]) {
+    let new_arr = arr.slice(mid + 1, len);
+    index = index + mid + 1;
+    return binary_serach(new_arr, x, index);
+  }
 }
 
-let sorted_arr = [1,2,3,3,3,4,4,6,9,10]
-let res = binary_serach(sorted_arr, -1, 0)
-console.log(res)
+let sorted_arr = [1, 2, 3, 3, 3, 4, 4, 6, 9, 10];
+let res = binary_serach(sorted_arr, 2);
+console.log(res);
