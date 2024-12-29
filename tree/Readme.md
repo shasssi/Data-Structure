1e3c22f0926ac3f35d3e73e007600681e8da9aef
shasssi <shashikumar942@gmail.com>
2024-12-29 22:45:06 +0530

    binary tree properties

--- tree/Readme.md
+++ tree/Readme.md
@@ -26,3 +26,32 @@
         - Data
         - pointer to the left child
         - pointer to the right child
+
+##### Binary Tree Properties
+    1. Maximun number of nodes at level 'l' of a binary tree is (2^l)
+        - level 0: 2^0 (1)
+        - level 1: 2^1 (2)
+        - level 3: 2^3 (8) ... level l: 2^l
+    2. maximun number of nodes in a binary tree of height 'h is (2^h -1)
+        - height 1: 2^1 - 1 (1) only root node
+        - height 2: 2^2 - 1 (3) root node + 2 children
+        - height 3: 2^3 - 1 (7) ... height h: 2^h - 1
+
+        - if height is considred as level and height starts from 0
+            - then formula will be 2^h+1 - 1
+    3. in a binary tree with N node, minimum possible height
+        - or minimum number of levels is ? log(base2)(N+1) - 1
+        - eg: if total 15 nodes: then level will be log2(15+1) - 1 -> 3 
+        - height will be log2(15+1) -> 4
+    4. in binary tree where every node has 0 or 2 childrens (means nodes with not 1 children)
+        - "number of leaf nodes" is always one more than nodes with two children.
+        eg:                      1
+                        2                 3
+                  4         5        6         7
+              8     9     10  11   12  13    14  15
+        - here total number of childs with 2 children = 7
+            - hence, number of leaf nodes will be one more than nodes with two children.
+            - number of leaf nodes = 7 + 1 = 8 (8 9 10 11 12 13 14 15)
+
+##### Reference
+    [https://www.youtube.com/watch?v=nbgty](https://www.youtube.com/watch?v=nbgtyBKn2tI&list=PLzjZaW71kMwQ-JABTOTypnpRk1BnD2Nx4)
--- tree/create-binary-tree.js
+++ tree/create-binary-tree.js
@@ -13,32 +13,50 @@ function printNode(n) {
     console.log(n.data);
     n = n.left;
   }
-  console.log('--------')
+  console.log("--------");
 }
 
-function printNodeAtLevel(n) {
-    // result should be in sequence of level 0,1,...n - eg - 1 2 3 4
-    if (n !== null) {
-        // print root
-        console.log(n.data);
-        let left = n.left;
-        let right = n.right;
-        while (left !== null || right !== null) {
-            // let sameLevelNodeElem = '';
-            if (left !== null) {
-                // sameLevelNodeElem += left.data;
-                console.log(left.data);
-                left = left.left
-            }
-            if (right !== null) {
-                // sameLevelNodeElem += ' ' + right.data;
-                console.log(right.data);
-                right = right.right
-            }
-            // console.log(sameLevelNodeElem);
-        }
+function printNodeAtLevel(n, h = 0) {
+  if (n !== null) {
+    // print root
+    console.log(n.data);
+    // left & right children
+    const left = n.left;
+    const right = n.right;
+    h = Math.max(h, h + 1);
+    console.log("height", h);
+    if (left) {
+      printNodeAtLevel(left, h);
     }
+    if (right) {
+      printNodeAtLevel(right, h);
+    }
+  }
 }
+
+// function printNodeAtLevel(n) {
+//     // result should be in sequence of level 0,1,...n - eg - 1 2 3 4
+//     if (n !== null) {
+//         // print root
+//         console.log(n.data);
+//         let left = n.left;
+//         let right = n.right;
+//         while (left !== null || right !== null) {
+//             // let sameLevelNodeElem = '';
+//             if (left !== null) {
+//                 // sameLevelNodeElem += left.data;
+//                 console.log(left.data);
+//                 left = left.left
+//             }
+//             if (right !== null) {
+//                 // sameLevelNodeElem += ' ' + right.data;
+//                 console.log(right.data);
+//                 right = right.right
+//             }
+//             // console.log(sameLevelNodeElem);
+//         }
+//     }
+// }
 
 (function init() {
   // make root node
@@ -47,8 +65,8 @@ function printNodeAtLevel(n) {
   root.right = new Node(3);
   root.left.left = new Node(4);
   root.left.right = new Node(6);
-  root.right.left = new Node(7)
-  root.right.right = new Node(5)
+  root.right.left = new Node(7);
+  root.right.right = new Node(5);
   /*
     NL - NULL
                 1
